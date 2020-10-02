9038acc95b37bc02a9dc099de72551af99b12a07
Erasmo0110 <erasmosenac@gmail.com>
2020-10-02 16:31:07 -0300

    Cadastrar Usuários

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { NavController } from '@ionic/angular';
 import { ActivatedRoute } from '@angular/router';
+import { AutenticacaoService } from '../services/autenticacao.service';
 
 @Component({
   selector: 'app-home',
@@ -8,9 +9,36 @@ import { ActivatedRoute } from '@angular/router';
   styleUrls: ['home.page.scss'],
 })
 export class HomePage {
-
+  
+  usuario: string;
   constructor(private nav: NavController,
-              private rota: ActivatedRoute  ) {}
+              private rota: ActivatedRoute,
+              private service: AutenticacaoService  ) {}
+
+
+
+  ngOnInit(){
+    this.service.detalhes().subscribe(res => {
+      if (res !== null){
+         this.usuario = res.email;
+      } else {
+        this.nav.navigateBack('');
+      }
+    }, err => {
+      console.log(err);
+    }
+      
+      )
+  } 
+
+  sair(){
+    this.service.logout().then(res => {
+      console.log(res);
+      this.nav.navigateBack('');
+    } ).catch(error => {
+      console.log(error);
+    })
+  }
 
   enviar(){
     console.log("cheguei aqui");
